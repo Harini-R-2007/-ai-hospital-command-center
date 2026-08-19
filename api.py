5a3455874e08120edfabbab058ae83280de4bb7b
harini-R-2007 <harinisekar205@gmail.com>
2026-08-19 21:50:46 +0530

    Complete working API for Render deployment

--- api.py
+++ api.py
@@ -156,3 +156,4 @@ def explain():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
+    # This is for Render deployment - Test
